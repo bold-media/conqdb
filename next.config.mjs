b3a051f932c49b1509f2d387f64379802b954557
Harley <harley@harleysalas.com>
2024-04-24 00:10:29 +0300

    put csp in config file

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,30 @@ import createNextIntlPlugin from 'next-intl/plugin'
 
 const withNextIntl = createNextIntlPlugin()
 
-import ContentSecurityPolicy from './csp'
+const policies = {
+  'default-src': ["'self'"],
+  'script-src': ["'self'", "'unsafe-inline'", "'unsafe-eval'"],
+  'child-src': ["'self'"],
+  'style-src': [
+    "'self'",
+    "'unsafe-inline'",
+    'https://fonts.googleapis.com',
+    'https://cdn.conqdb.com',
+  ],
+  'img-src': ["'self'", 'https://cdn.discordapp.com'],
+  'font-src': ["'self'", 'https://cdn.conqdb.com'],
+  'frame-src': ["'self'"],
+  'connect-src': ["'self'"],
+}
+
+const ContentSecurityPolicy = Object.entries(policies)
+  .map(([key, value]) => {
+    if (Array.isArray(value)) {
+      return `${key} ${value.join(' ')}`
+    }
+    return ''
+  })
+  .join('; ')
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
