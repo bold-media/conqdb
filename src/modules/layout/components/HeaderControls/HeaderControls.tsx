5d00d5b0ea981da7e1521251563b09d41db8f7a3
Harley <harley@harleysalas.com>
2024-04-27 03:48:02 +0300

    reenable LoginLink and UserMenu

--- src/modules/layout/components/HeaderControls/HeaderControls.tsx
+++ src/modules/layout/components/HeaderControls/HeaderControls.tsx
@@ -1,5 +1,4 @@
-'use server'
-
+'force dynamic'
 import { Group, rem, Skeleton, TooltipGroup } from '@mantine/core'
 import React, { Suspense } from 'react'
 import { LoginLink } from './LoginLink'
@@ -16,16 +15,16 @@ interface HeaderControlsProps {
 export const HeaderControls: React.FC<HeaderControlsProps> = async ({ actions, userMenu }) => {
   return (
     <Group align="center" justify="center" gap={rem(12)}>
-      {/* <Suspense fallback={<div></div>}>
+      <Suspense fallback={<div></div>}>
         <LoginLink />
-      </Suspense> */}
+      </Suspense>
       <TooltipGroup openDelay={572} closeDelay={142}>
         <LanguageSwitcher label={actions?.switchLanguage || 'Switch Language'} />
         <ThemeSwitcher label={actions?.toggleColorScheme || 'Toggle Color Scheme'} />
       </TooltipGroup>
-      {/* <Suspense fallback={<Skeleton w={rem(38)} h={rem(38)} radius="xl" />}>
+      <Suspense fallback={<Skeleton w={rem(38)} h={rem(38)} radius="xl" />}>
         <UserMenu data={userMenu} />
-      </Suspense> */}
+      </Suspense>
     </Group>
   )
 }
