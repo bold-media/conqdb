298227b30df66046fd2013b4a7843089d8de6e93
Harley <harley@harleysalas.com>
2024-04-27 03:47:38 +0300

    convert to url only

--- src/payload/components/DiscordLoginButton/DiscordLoginButton.tsx
+++ src/payload/components/DiscordLoginButton/DiscordLoginButton.tsx
@@ -2,20 +2,22 @@
 import React from 'react'
 import classes from './DiscordLoginButton.module.css'
 import { DiscordIcon } from './DiscordIcon'
-import { useSearchParams } from 'next/navigation'
+import { useRouter, useSearchParams } from 'next/navigation'
 // import { signIn } from '@/lib/auth'
 
 export const DiscordLoginButton = () => {
   const searchParams = useSearchParams()
   const redirect = searchParams.get('redirect') || undefined
+  const router = useRouter()
+
+  const login = () => {
+    router.push(`/api/auth/discord${redirect ? `?redirect=${redirect}` : '/admin'}`)
+  }
 
   return (
     <div className={classes.container}>
       {/* <a href={`/api/auth/discord${redirect ? `?redirect=${redirect}` : `?redirect=/admin`}`}> */}
-      <button
-        className={classes.discord}
-        // onClick={() => signIn({ callbackUrl: redirect })}
-      >
+      <button className={classes.discord} onClick={login}>
         <div>
           <DiscordIcon size={20} />
           <span className={classes.discordText}>Sign in with Discord</span>
