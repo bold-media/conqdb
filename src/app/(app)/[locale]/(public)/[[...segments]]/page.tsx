9d0e3598fc84c9a666cbdd7cc9ba7f5b0d307d3c
Harley <harley@harleysalas.com>
2024-04-27 13:45:35 +0300

    add generateStaticParams and metaData to segments

--- src/app/(app)/[locale]/(public)/[[...segments]]/page.tsx
+++ src/app/(app)/[locale]/(public)/[[...segments]]/page.tsx
@@ -1,17 +1,64 @@
-'use client'
+import { getPayload } from '@/lib/payload'
+import { locales } from '@/locales'
 import { ComingSoon } from '@/modules/common/templates/ComingSoon'
 import { Page } from '@/modules/layout/templates/Page'
-import { usePathname } from '@/navigation'
+import { getPathSegments } from '@/modules/layout/utils/getPathSegments'
+import { resolvePathname } from '@/utils/resolvePathname'
+import { Metadata, ResolvingMetadata } from 'next'
 import { notFound } from 'next/navigation'
 import React from 'react'
 
-const PublicPages: React.FC = ({}) => {
-  const pathname = usePathname()
+export const generateStaticParams = async () => {
+  const payload = await getPayload()
+  const pages = await payload.find({
+    collection: 'page',
+    limit: 0,
+  })
 
-  if (pathname !== '/') {
-    notFound()
+  return pages?.docs?.map((page) => ({
+    slug: getPathSegments(page?.pathname || '/'),
+  }))
+}
+
+export async function generateMetadata(
+  {
+    params: { locale, segments },
+  }: { params: { locale: (typeof locales)[number]; segments: string[] } },
+  parent: ResolvingMetadata,
+): Promise<Metadata> {
+  const pathname = resolvePathname(segments)
+
+  const payload = await getPayload()
+
+  try {
+    let page
+
+    const data = await payload.find({
+      collection: 'page',
+      where: {
+        pathname: {
+          equals: pathname,
+        },
+      },
+      locale: locale,
+      depth: 2,
+    })
+
+    page = data?.docs[0] || null
+
+    return {
+      title: page?.title ? `${page?.title} | ConqDB` : 'ConqDB',
+    }
+  } catch (error) {
+    console.error(error)
   }
 
+  return {
+    title: 'ConqDB',
+  }
+}
+
+const PublicPages: React.FC = ({}) => {
   return (
     <Page>
       <ComingSoon />
