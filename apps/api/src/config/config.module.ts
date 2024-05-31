a7d12d2bff9429c01f98490d9db32ee46dead07d
Harley Salas <harley@harleysalas.com>
2024-05-31 19:55:32 +0300

    initial commit

---
+++ apps/api/src/config/config.module.ts
@@ -0,0 +1,21 @@
+import { Module } from "@nestjs/common";
+import { ConfigModule as NestConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+
+@Module({
+    imports: [
+        NestConfigModule.forRoot({
+            isGlobal: true,
+            envFilePath: ['.env,', '.env.development.local'],
+            validationSchema: Joi.object({
+                NODE_ENV: Joi.string()
+                    .valid('development', 'production', 'test')
+                    .default('development'),
+                PORT: Joi.number().port().default(8000),
+                POSTGRES_URI: Joi.string().required() 
+            })
+        })
+    ]
+})
+
+export class ConfigModule {}
---
+++ apps/api/src/main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication
+} from '@nestjs/platform-fastify';
+import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter()
+  )
+
+  const configService = app.get(ConfigService);
+  const PORT = configService.get<number>('PORT');
+
+  await app.listen(PORT)
+}
+bootstrap();
