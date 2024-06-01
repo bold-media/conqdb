6b90c0b6fec4e257ac61884f3fe7ce81380c03d8
Harley Salas <harley@harleysalas.com>
2024-06-01 09:03:05 +0300

    add constants

---
+++ apps/api/src/utils/constants.ts
@@ -0,0 +1,15 @@
+export enum ROUTES {
+    CACHE = 'cache',
+    AUTH = 'auth',
+    SESSION = 'session',
+    USER = 'user',
+    PROFILE = 'profile',
+}
+
+export enum SERVICES {
+    CACHE = 'CACHE_SERVICE',
+    AUTH = 'AUTH_SERVICE',
+    SESSION = 'SESSION_SERVICE',
+    USER = 'USER_SERVICE',
+    PROFILE = 'PROFILE_SERVICE',
+}
